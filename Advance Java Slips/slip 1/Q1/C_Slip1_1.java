3be3e2dabf4da860b4df7b6ccfeb648cdf3a33ea
RAHUL <rahulmistri1997@gmail.com>
2020-09-17 21:49:36 +0530

    Modified Adv Java Slip1Q1

--- Advance Java Slips/slip 1/Q1/C_Slip1_1.java
+++ Advance Java Slips/slip 1/Q1/C_Slip1_1.java
@@ -1,12 +1,11 @@
-
-//CLIENT:
 import java.io.*;
 import java.net.*;
 
-class C_Slip1_1 {
+class C_Slip1_1{
     public static void main(String a[]) throws Exception {
-        Socket ss = new Socket("localhost", 1000);
-        System.out.println("IP Address = " + ss.getInetAddress());
-        System.out.println("Client port = " + ss.getPort());
+        Socket ss = new Socket("localhost",1000);
+        System.out.println("IP address = " + ss.getInetAddress());
+        System.out.println("Port = " + ss.getPort());
+        System.out.println("Name = " + ss.getInetAddress().getHostName());
     }
 }
